05cd0ea4731da89f183cd66b99eae6c4c9fe2f55
HoangLong <nguyenphamhoanglong2000@gmail.com>
2019-11-17 18:27:34 +0700

    DeAn

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdView = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grdView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdView
+            // 
+            this.grdView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdView.Location = new System.Drawing.Point(0, 134);
+            this.grdView.Name = "grdView";
+            this.grdView.Size = new System.Drawing.Size(332, 128);
+            this.grdView.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(71, 85);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add Goods";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 261);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.grdView);
+            this.Name = "Form1";
+            this.Text = "Goods";
+            ((System.ComponentModel.ISupportInitialize)(this.grdView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdView;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+            btnAdd.Click += btnAdd_Click;
+            this.grdView.KeyDown += grdView_KeyDown;
+        }
+
+        void grdView_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == 46)
+            {
+                if (this.grdView.SelectedRows.Count == 1)
+                {
+                    if (MessageBox.Show("Do you want to delete this") == System.Windows.Forms.DialogResult.OK)
+                    {
+                        SetUp selected = (SetUp)this.grdView.SelectedRows[0].DataBoundItem;
+                        var db = new QuanLyHHEntities();
+                        QuanLyHH deleted = db.QuanLyHHs.Find(selected.id);
+                        db.QuanLyHHs.Remove(deleted);
+                        db.SaveChanges();
+                        this.form1();
+                    }
+                }
+            }
+        }
+
+        void btnAdd_Click(object sender, EventArgs e)
+        {
+            (new AddGoods()).ShowDialog();
+            this.form1();
+        }
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            this.form1();
+        }
+
+      public void form1()
+        {
+            var db = new QuanLyHHEntities();
+            var view = db.QuanLyHHs.ToArray();
+            this.grdView.DataSource = view;
+        }
+    }
+}
